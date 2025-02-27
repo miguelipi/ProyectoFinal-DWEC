3ca8558baf23db0b090e6d0b6d5907c40348370a
miguelipi <miguel06ipi@gmail.com>
2025-02-27 13:41:12 +0100

    Video

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,10 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from '../components/Header';
+import Video from '../components/Video';
 
 export default function Home() {
-    return (
-        <>  
-          <Header></Header>
-        </>
-    );
+  return (
+    <>
+      <Header></Header>
+      <main>
+        <Video></Video>
+      </main>
+    </>
+  );
 }
